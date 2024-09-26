59166ee38f96b918f26f8ec574436682785cc74f
micropenis <att33pod@gmail.com>
2024-09-26 21:43:36 +0700

    History commit
    All files to view - here. When someone from 2 components will be newer, i'll update it again

---
+++ cs/jellybins/Models/MachObjectHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jellybins.Models
+{
+    // Mach microkernel model for Apple binaries/executables
+    internal class MachObjectHeader
+    {
+    }
+}
