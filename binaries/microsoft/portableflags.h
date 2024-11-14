140e172d2d3db69093b7f91c81be99c7e7fcf0c3
Alexey Tolstopyatov <att33pod@gmail.com>
2024-11-14 17:31:55 +0700

    partically deleting old codebase

--- binaries/microsoft/portableflags.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by MagicBook on 30.09.2024.
-//
-
-#ifndef JBC_PORTABLEFLAGS_H
-#define JBC_PORTABLEFLAGS_H
-
-#endif //JBC_PORTABLEFLAGS_H
--- binaries/unix/elftypes.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by MagicBook on 30.09.2024.
-//
-
-#ifndef JBC_ELFTYPES_H
-#define JBC_ELFTYPES_H
-
-
-#endif //JBC_ELFTYPES_H
