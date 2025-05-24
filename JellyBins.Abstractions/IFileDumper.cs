f3ebd95f1d47ca1526439d81bd567113b6cc814d
Alexey Tolstopyatov <att33pod@gmail.com>
2025-05-24 18:01:02 +0700

    CLI pattern matching

--- JellyBins.Abstractions/IFileDumper.cs
+++ JellyBins.Abstractions/IFileDumper.cs
@@ -2,8 +2,15 @@
 
 public interface IFileDumper
 {
+    /// <summary>
+    /// Makes dump of required (set in constructor method)
+    /// binary
+    /// </summary>
     void Dump();
     UInt16 GetExtensionTypeId();
     UInt16 GetBinaryTypeId();
+    /// <summary>
+    /// JellyBins internal type flag of determined binary
+    /// </summary>
     FileSegmentationType SegmentationType { get; }
 }
--- JellyBins.Console/Program.fs
+++ JellyBins.Console/Program.fs
@@ -1,81 +1,153 @@
 ﻿module JellyBins.Console
     open System
     open System.Diagnostics
+    open System.IO
     open System.Reflection
+    open FSharp.Core
+    open JellyBins.Abstractions
+    open JellyBins.Core.Factories
+    open JellyBins.NewExecutable.Models
     type AppCommand =
-        | AppDump of options: DumpOptions
+        | AppDump of dOptions: DumpOptions
+        | AppInfo of iOptions: InfoOption
+        | AppInvalid of message: String
         | AppHelp
         | AppVersion
-        | AppInvalidCommand of message: String
     and DumpOptions = {
         FilePath: String
         Verbose: bool
-        Sections: List<String>
+        Sections: bool
+        Imports: bool
+        Exports: bool
+        Headers: bool
     }
+    and InfoOption = {
+        FilePath: string
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     let (|AppCommand|_|) (args: string list) =
         match args with
         | "dump" :: rest -> Some rest
-        | "help" :: _ -> Some []
-        | "version" :: _ -> Some []
+        | "help" :: _ -> Some []    // empty list
+        | "version" :: _ -> Some [] // empty list
+        | "info" :: _ -> Some []    // empty list
         | _ -> None
+    /// <summary>
+    /// <see cref="AppCommand"/> has <c>--verbose</c> flag
+    /// </summary>
     let (|VerboseFlag|_|) = function
         | "--verbose" :: rest -> Some (true, rest)
+        | "-v" :: rest -> Some (true, rest)
         | rest -> Some (false, rest)
-
-    let (|SectionsFlag|_|) = function
-        | "--sections" :: sections :: rest ->
-            Some (sections.Split(',') |> List.ofArray, rest)
-        | rest -> Some ([], rest)
+    /// <summary>
+    /// <see cref="AppCommand"/> has <c>--section</c> flag
+    /// </summary>
+    let (|SectionsFlag|) = function
+        | "--sections" -> Some true
+        | "-s" -> Some true
+        | _ -> Some false
+    let (|ImportsFlag|) = function
+        | "--imports" -> Some true
+        | "-it" -> Some true
+        | _ -> Some false
+    let (|ExportsFlag|) = function
+        | "--exports" -> Some true
+        | "-et" -> Some true
+        | _ -> Some false
+    let (|Info|) = function
+        | "-i" -> Some true
+        | _ -> Some false
+        
     [<CompiledName "ParseDumpOptions">]
     let rec parseDumpOptions args =
         match args with
         | filePath :: rest ->
             let opts = {
+                // declare ALL available options
                 FilePath = filePath
                 Verbose = false
-                Sections = []
+                Sections = false
+                Imports = false
+                Exports = false
+                Headers = false 
             }
             parseDumpOptionsRest opts rest
-        | _ -> AppInvalidCommand "Missing file path"
+        | _ -> AppInvalid "Missing file path"
     and parseDumpOptionsRest opts args =
         match args with
         | VerboseFlag (verbose, rest) ->
-            parseDumpOptionsRest { opts with Verbose = verbose } rest
-        | SectionsFlag (sections, rest) ->
-            parseDumpOptionsRest { opts with Sections = sections } rest
+            parseDumpOptionsRest {
+                opts with Verbose = verbose
+            } rest
+     // | Sections idea missed
         | [] -> AppDump opts
-        | unexpected -> AppInvalidCommand $"Unexpected arguments: %A{unexpected}"
+        | unexpected -> AppInvalid $"Unexpected arguments: %A{unexpected}"
 
+    /// <summary>
+    /// Translates items of <c>argv</c> to F# Atoms
+    /// </summary>
+    /// <param name="argv">Vector of Command line arguments</param>
     [<CompiledName "ParseCommand">]
     let parseCommand (argv: string[]) =
         let args = argv
-                   |> List.ofArray
-        
+                   |> Array.toList
         match args with
-        | AppCommand ["dump"] -> AppInvalidCommand "Missing file path for dump command"
-        | AppCommand ("dump" :: rest) -> parseDumpOptions rest
-        | AppCommand ["help"] -> AppHelp
-        | AppCommand ["version"] -> AppVersion
-        | [] -> AppHelp  // Если нет аргументов - показываем помощь
-        | _ -> AppInvalidCommand "Unknown command"
+        | ["dump"] -> AppInvalid "Missing file path for dump command"
+        | ("dump" :: rest) -> parseDumpOptions rest
+        | ["help"] -> AppHelp
+        | ["version"] -> AppVersion
+        | [] -> AppHelp
+        | cmdList -> AppInvalid $"Unknown command sequence: %s{cmdList |> string}"
+    
+    let printDump (d: DumpOptions) =
+        if d.Verbose then
+            printfn $"Dumping file: {d.FilePath}"
+            printfn $"Size:\t %d{FileInfo(d.FilePath).Length * int64 1024}K"
+        // get ready for totally dumping
+        // let dumper = FileDumperFactory.CreateInstance d.FilePath
+        // match dumper.SegmentationType with
+        //     | FileSegmentationType.NewExecutable ->
+        //         dumper.Dump()
+        //         // C# part for tabes drawing (saving segmentation as Dictionaries)
+        //     | _ -> AppInvalid "Unknown binary (NotSupportedException)"
+        //            |> ignore
+        //            
+        ()
+    
+    /// <summary>
+    /// Executes translated command
+    /// </summary>
+    /// <param name="command"></param>
     [<CompiledName "HandleCommand">]
     let rec handleCommand (command: AppCommand) =
         match command with
-        | AppDump opts ->
-            printfn $"Dumping file: {opts.FilePath}"
-            printfn $"Verbose: {opts.Verbose}"
-            printfn $"Sections: {opts.Sections}"
-            // DumperFactory.CreateInstance(opts.FilePath).Dump()
         | AppHelp ->
-            printfn "Usage:"
-            printfn "  dump <file> [--format json|text|md] [--verbose] [--sections section1,section2]"
-            printfn "  help"
-            printfn "  version"
+            printfn "Usage: " // 2 paragraphs
+            printfn "  dump [--section] [--headers] [--imports] [--exports] [--info]"
+            printfn "  dump [-s] [-h] [-it] [-et] [-i]"
+            printfn "  version -- Prints information about this Assembly"
+            printfn "  help    -- Prints this page"
+            printfn "  info    -- Prints table of common dumping results"
+            exit(0) 
         | AppVersion ->
-            printfn $"JellyBins {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().FullName)}"
-        | AppInvalidCommand msg ->
-            printfn $"Error: {msg}"
+            printfn "\nJellyBins (C) CoffeeLake 2024-2025" // 2 paragraphs
+            printfn "GUI-less application variant for dumping binaries"
+            printfn $"  File version: %s{FileVersionInfo.GetVersionInfo(
+                Assembly.GetCallingAssembly().Location).FileVersion |> string}"
+            printfn $"  Product version: %s{FileVersionInfo.GetVersionInfo(
+                Assembly.GetCallingAssembly().Location).ProductVersion |> string}"
+            printfn ""
+            exit(0)
+        | AppDump opts ->
+            printDump opts
+            exit(0)
+        | AppInfo opts ->
             handleCommand AppHelp
+        | AppInvalid msg ->
+            printfn $"Error: {msg}\n"
     
     [<EntryPoint>]
     let main argv =
